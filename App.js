0f52efb569c9170c1632c8f2798b11e863bd16ed
Kirby McKenzie <kirby@workplacearcade.com>
2021-04-05 13:47:43 +1000

    Add files

---
+++ App.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import {WebView} from 'react-native-webview';
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <View style={{flex: 1}}>
+      <WebView
+        originWhitelist={['*']}
+        source={{uri: 'https://dev.geobuff.com'}}
+        style={{marginTop: 40}}
+      />
+    </View>
+  );
+};
+
+export default App;
