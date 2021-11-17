160774b7f1dbb4012a0e631694cc6e38eadf529a
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2021-11-17 20:08:34 +1100

    Update Android app to remove action bar and to remove webview zoom

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import React, { useEffect } from 'react';
 import { WEBVIEW_URL } from '@env';
-import { SafeAreaView, StyleSheet, View, StatusBar } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  StatusBar,
+  Platform,
+} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 
 import { WebView } from 'react-native-webview';
+
+const isAndroid = Platform.OS === 'android';
+
+const INJECTEDJAVASCRIPT =
+  "const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=0.99, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); ";
 
 const App = () => {
   useEffect(() => {
@@ -20,7 +31,12 @@ const App = () => {
     <View style={styles.flex}>
       <SafeStatusBar />
       <View style={styles.flex}>
-        <WebView originWhitelist={['*']} source={{ uri: WEBVIEW_URL }} />
+        <WebView
+          originWhitelist={['*']}
+          source={{ uri: WEBVIEW_URL }}
+          scrollEnabled={isAndroid ? false : true}
+          injectedJavaScript={isAndroid ? INJECTEDJAVASCRIPT : undefined}
+        />
       </View>
     </View>
   );
@@ -29,6 +45,7 @@ const App = () => {
 const styles = StyleSheet.create({
   flex: {
     flex: 1,
+    marginTop: isAndroid ? 12 : undefined,
   },
 });
 
